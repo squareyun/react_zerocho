e70d3877e297ac9c716e32c9f9a712ef85eb5b58
squareyun <squareyun@gmail.com>
2022-01-31 21:20:24 +0900

    웹팩 데브 서버와 핫 리로딩

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: "wordrelay-setting",
@@ -27,14 +28,27 @@ module.exports = {
                         }],
                         '@babel/preset-react',
                     ],
-                    plugins: ["@babel/plugin-proposal-class-properties"],
+                    plugins: [
+                        "@babel/plugin-proposal-class-properties",
+                        'react-refresh/babel',
+                    ],
                 },
             },
         ],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
 
     output: {
         path: path.join(__dirname, "dist"),
         filename: "app.js",
+        publicPath: '/dist',
+    },
+
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true,
     },
 };
