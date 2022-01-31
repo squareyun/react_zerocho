eb3b8bc7dd3f928b2619a3485302b1e247c3fcd4
squareyun <squareyun@gmail.com>
2022-01-31 12:18:47 +0900

    구구단 웹팩으로 빌드

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,32 +1,32 @@
 const path = require("path");
 
 module.exports = {
-  name: "wordrelay-setting",
-  mode: "development", // 실서비스: production
-  devtool: "eval", // 빠르게
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+    name: "wordrelay-setting",
+    mode: "development", // 실서비스: production
+    devtool: "eval", // 빠르게
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
 
-  entry: {
-    app: ["./client"],
-  }, // 입력
+    entry: {
+        app: ["./client"],
+    }, // 입력
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
-        },
-      },
-    ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-proposal-class-properties"],
+                },
+            },
+        ],
+    },
 
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "app.js",
-  }, // 출력
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "app.js",
+    },
 };
