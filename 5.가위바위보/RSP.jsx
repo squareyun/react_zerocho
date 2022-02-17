d2b65ce33f6ffc9dc6212a067cdd6bfcda8157da
squareyun <squareyun@gmail.com>
2022-02-17 22:28:13 +0900

    가위바위보 게임만들기

--- 5.가위바위보/RSP.jsx
+++ 5.가위바위보/RSP.jsx
@@ -8,42 +8,76 @@ const rspCoords = {
     바위: '0', 가위: '-142px', 보: '-284px',
 };
 
+const scores = {
+    가위: 1, 바위: 0, 보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function (v) {
+        return v[1] === imgCoord;
+    })[0];
+};
+
 class RSP extends Component {
     state = {
-        result: '', imgCoord: 0, scord: 0,
+        result: '', imgCoord: 0, score: 0,
     };
 
     interval;
 
     componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 함
-        this.interval = setInterval(() => {
-            const {imgCoord} = this.state; // 클로저 문제 조심. 비동기함수가 바깥에 있는 변수를 참조할 때 발생
-
-            if (imgCoord == rspCoords.바위) {
-                this.setState({
-                    imgCoord: rspCoords.가위,
-                });
-            } else if (imgCoord == rspCoords.가위) {
-                this.setState({
-                    imgCoord: rspCoords.보,
-                });
-            } else if (imgCoord == rspCoords.보) {
-                this.setState({
-                    imgCoord: rspCoords.바위,
-                });
-            }
-        }, 1000);
-    }
-
-    componentDidUpdate() { // 리렌더링 후
-    }
+        this.interval = setInterval(this.changeHand, 100);
+    };
 
     componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 함
         clearInterval(this.interval);
-    }
+    };
 
-    onClickBtn() {
-    }
+    changeHand = () => {
+        const {imgCoord} = this.state; // 클로저 문제 조심. 비동기함수가 바깥에 있는 변수를 참조할 때 발생
+
+        if (imgCoord == rspCoords.바위) {
+            this.setState({
+                imgCoord: rspCoords.가위,
+            });
+        } else if (imgCoord == rspCoords.가위) {
+            this.setState({
+                imgCoord: rspCoords.보,
+            });
+        } else if (imgCoord == rspCoords.보) {
+            this.setState({
+                imgCoord: rspCoords.바위,
+            });
+        }
+    };
+
+    onClickBtn = (choice) => () => {
+        const {imgCoord} = this.state;
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            this.setState({
+                result: '비겼습니다.',
+            });
+        } else if ([-1, 2].includes(diff)) {
+            this.setState((prevState) => {
+                return {
+                    result: '이겼습니다!', score: prevState.score + 1,
+                };
+            });
+        } else {
+            this.setState((prevState) => {
+                return {
+                    result: '졌습니다.', score: prevState.score - 1,
+                };
+            });
+        }
+        setTimeout(() => {
+            this.interval = setInterval(this.changeHand, 100);
+        }, 2000);
+    };
 
     render() {
         const {score, result, imgCoord} = this.state;
