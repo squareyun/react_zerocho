0a7758916d9827d36d258ee694e74517d339c324
squareyun <squareyun@gmail.com>
2022-02-19 16:36:13 +0900

    componentDidUpdate

--- 6.로또/Lotto.jsx
+++ 6.로또/Lotto.jsx
@@ -23,7 +23,8 @@ class Lotto extends Component {
 
     timeouts = [];
 
-    componentDidMount() {
+    runTimeouts = () => {
+        console.log('runTimeouts');
         const {winNumbers} = this.state;
         for (let i = 0; i < winNumbers.length - 1; i++) {
             this.timeouts[i] = setTimeout(() => {
@@ -40,14 +41,37 @@ class Lotto extends Component {
                 redo: true,
             })
         }, 7000);
-    }
+    };
+
+    componentDidMount() {
+        console.log('didMount');
+        this.runTimeouts();
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('didUpdate');
+        if (this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+    };
 
     // 메모리 누수 문제 꼭 신경쓰자.
     componentWillUnmount() {
         this.timeouts.forEach((v) => {
             clearTimeout(v);
         });
-    }
+    };
+
+    onClickRedo = () => {
+        console.log('onClickRedo');
+        this.setState({
+            winNumbers: getWinNumbers(), // 당첨 숫자들
+            winBalls: [],
+            bonus: null, // 보너스 공
+            redo: false,
+        });
+        this.timeouts = [];
+    };
 
     render() {
         const {winBalls, bonus, redo} = this.state;
