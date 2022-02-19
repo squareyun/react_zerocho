c09a27002b067e09044a9001894e9a4ba9cc77b8
squareyun <squareyun@gmail.com>
2022-02-19 12:04:04 +0900

    setTimeout 여러 번 사용하기

--- 6.로또/Lotto.jsx
+++ 6.로또/Lotto.jsx
@@ -21,17 +21,46 @@ class Lotto extends Component {
         redo: false,
     };
 
+    timeouts = [];
+
+    componentDidMount() {
+        const {winNumbers} = this.state;
+        for (let i = 0; i < winNumbers.length - 1; i++) {
+            this.timeouts[i] = setTimeout(() => {
+                this.setState((prevState) => {
+                    return {
+                        winBalls: [...prevState.winBalls, winNumbers[i]],
+                    };
+                });
+            }, (i + 1) * 1000);
+        }
+        this.timeouts[6] = setTimeout(() => {
+            this.setState({
+                bonus: winNumbers[6],
+                redo: true,
+            })
+        }, 7000);
+    }
+
+    // 메모리 누수 문제 꼭 신경쓰자.
+    componentWillUnmount() {
+        this.timeouts.forEach((v) => {
+            clearTimeout(v);
+        });
+    }
+
     render() {
-        const { winBalls, bonus, redo } = this.state;
+        const {winBalls, bonus, redo} = this.state;
         return (
             <>
                 <div>당첨 숫자</div>
                 <div id="결과창">
-                    {winBalls.map((v) => <Ball key={v} number={v} />)}
+                    {winBalls.map((v) => <Ball key={v} number={v}/>)}
                 </div>
                 <div>보너스!</div>
-                {bonus && <Ball number={bonus} />}
-                <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+                {bonus && <Ball number={bonus}/>}
+                {redo && <button onClick={this.onClickRedo}>한 번 더!
+                </button>}
             </>
         );
     };
